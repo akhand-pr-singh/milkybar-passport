e38b3d6a8d2ad31d6443497da7e683bf55134061
Akhand <akhndprtp@gmail.com>
2023-09-11 07:12:52 +0530

    Final Changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,15 +2,23 @@ import React, { useRef, useState } from 'react';
 import '../css/main.css';
 import Button from './Button';
 import Webcam from 'react-webcam';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 function Main() {
+
     const webcamRef = useRef(null);
+    const pdfRef = useRef();
+
+
     const [capturedImage, setCapturedImage] = useState(null);
     const [visible, setVisible] = useState(false);
     const [show, setShow] = useState(false);
     const [name, setName] = useState('NAME');
     const [error, setError] = useState('');
 
+
+    //WEBCAM CAPTURING IMAGE
     const capture = () => {
         const imageSrc = webcamRef.current.getScreenshot();
         setCapturedImage(imageSrc);
@@ -20,10 +28,14 @@ function Main() {
         setCapturedImage(null);
     }
 
+
     const handleFallbackClick = () => {
         setVisible(true);
     }
 
+
+
+    //FORM VALIDATIONS
     const handleChange = (event) => {
         const value = event.target.value;
         const lettersOnly = /^[A-Za-z\s]+$/;
@@ -43,13 +55,34 @@ function Main() {
         console.log(error);
     }
 
+    //DOWNLOADING AS PDF FUNCTIONALITY
+    const generatePDF = () => {
+        const input = pdfRef.current;
+        html2canvas(input).then((canvas) => {
+            const imgData = canvas.toDataURL('image/png');
+            const pdf = new jsPDF('p', 'mm', 'a4', true);
+            const pdfWidth = pdf.internal.pageSize.getWidth();
+            const pdfHeight = pdf.internal.pageSize.getHeight();
+            const imgWidth = canvas.width;
+            const imgHeight = canvas.height;
+            const ratio = Math.min(pdfWidth / imgWidth, pdfHeight / imgHeight);
+            const imgX = (pdfWidth - imgWidth * ratio) / 2;
+            const imgY = 30;
+            pdf.addImage(imgData, 'PNG', imgX, imgY, imgWidth * ratio, imgHeight * ratio);
+            pdf.save('passport.pdf');
+        });
+    };
+
+
+
     return (
         <>
             <div className='main'>
 
+                {/* LANDING PAGE */}
                 <div className="landing-page">
                     <img className='position-absolute' src="./images/background.png" alt="" />
-                    <div className="passport">
+                    <div className="passport" ref={pdfRef}>
                         <div className="mypassport">
                             <img src="./images/brand-logo.png" alt="logo" />
                             <img src="./images/earth.png" alt="earth" />
@@ -64,7 +97,7 @@ function Main() {
                                 <div className="information">
                                     <h6>Name</h6>
                                     {!error ? (<h5 onClick={() => setVisible(true)}>{name}</h5>) : (<h5 onClick={() => setVisible(true)}>Invalid</h5>)}
-                                    <h6>I'M READY TO DISCOVER THE WORLD!</h6>
+                                    <p><b>I'M READY TO DISCOVER THE WORLD!</b></p>
                                 </div></div>
                             <div className="passport-bottom-rectangle"></div>
                             <div className="passport-bottom">
@@ -73,9 +106,12 @@ function Main() {
                             </div>
                         </div>
                     </div>
-                    <img className='position-absolute download' src="./images/download.png" alt="download" />
+                    <img className='position-absolute download' type='button' onClick={generatePDF} src="./images/download.png" alt="download" />
                 </div>
 
+
+
+                {/* WEBCAM PAGE LOGIC*/}
                 {visible ?
                     (<div className="capture-page">
                         <header>
@@ -105,8 +141,10 @@ function Main() {
                                 <img src="./images/save.png" role='button' onClick={save} className='save' alt="" />
                             </div>
                         </div>
-                        <img className='position-absolute download' src="./images/download.png" alt="download" />
+                        <img className='position-absolute download' type='button' onClick={generatePDF} src="./images/download.png" alt="download" />
                     </div>) : (<div></div>)}
+
+
             </div >
         </>
     )
